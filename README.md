017f7628d337fff7bf6ac6be4a51d57b96189b72
Yunus Bozkurt <yunusbozkurt7000@gmail.com>
2022-03-09 06:35:34 +0000

    Deploy questionaire to iphone

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 Basic Flutter Application
 
 [Flutter Crash Course for Beginners 2021 - Build a Flutter App with Google's Flutter & Dart](https://www.youtube.com/watch?v=x0uinJvhNxI)
+
+## How to run flutter app
+
+    flutter run
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,45 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer chosen!');
+  }
+
+  @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favourite colour?',
+      'What\'s your favourite animal?'
+    ];
     return MaterialApp(
-      home: Text('Hello!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Text('The question!'),
+            ElevatedButton(
+              child: Text('Answer 1'),
+              onPressed: () => print('Answer 1 chosen!'),
+            ),
+            ElevatedButton(
+              child: Text('Answer 2'),
+              onPressed: () => print('Answer 2 chosen!'),
+            ),
+            ElevatedButton(
+              child: Text('Answer 3'),
+              onPressed: () => print('Answer 3 chosen!'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
