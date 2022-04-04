000a5b815efdc9043c020f2f4de3d05432b043ff
Yunus Bozkurt <yunusbozkurt7000@gmail.com>
2022-04-04 21:04:57 +0100

    figma to flutter trial

--- lib/main.dart
+++ lib/main.dart
@@ -1,116 +1,40 @@
 import 'package:flutter/material.dart';
-
-// void main() => runApp(MyApp());
+import 'package:flutterapp/flutter_appapp/generatedcruscottodellacantinawidget/GeneratedCRUSCOTTODELLACANTINAWidget.dart';
+import 'package:flutterapp/flutter_appapp/generatedfooterwidget/GeneratedFooterWidget.dart';
+import 'package:flutterapp/flutter_appapp/generatedaddwinewidget/GeneratedAddWineWidget.dart';
+import 'package:flutterapp/flutter_appapp/generatededitwinewidget/GeneratedEditWineWidget.dart';
+import 'package:flutterapp/flutter_appapp/generatedwinewidget2/GeneratedWineWidget2.dart';
+import 'package:flutterapp/flutter_appapp/generatedwinelistwidget/GeneratedWineListWidget.dart';
+import 'package:flutterapp/flutter_appapp/generatedwinebottlewidget6/GeneratedWineBottleWidget6.dart';
+import 'package:flutterapp/flutter_appapp/generatedfooterwidget5/GeneratedFooterWidget5.dart';
+import 'package:flutterapp/flutter_appapp/generatedhomeheaderwidget/GeneratedHomeHeaderWidget.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(flutter_appApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    // todo
-    return MyAppState();
-  }
-}
-
-class MyAppState extends State<MyApp> {
-  final List<int> id = <int>[1, 4, 10, 12, 2, 9];
-  final List<String> winery = <String>[
-    'Tentue Salva Terra',
-    'Belmonte',
-    'Brunelli',
-    'Brunelli Campo Inferi',
-    'Domini Veneti',
-    'El Coston',
-  ];
-  final List<String> area = <String>[
-    'Classico',
-    '',
-    'San Pietro',
-    'Negrar',
-    'Domini Veneti',
-    'Monte',
-  ];
-  final List<int> vintage = <int>[2011, 0, 2016, 2015, 2015, 0];
-  final List<int> release = <int>[2016, 0, 2020, 2020, 2018, 0];
-  final List<String> price = <String>[
-    "£32.00",
-    "£0.00",
-    "£28.00",
-    "£50.00",
-    "£23.00",
-    "£50.00",
-  ];
+class flutter_appApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.black,
-        appBar: AppBar(
-          backgroundColor: Colors.purple.shade800,
-          title: const Text(
-            'Wine Inventory',
-            style: TextStyle(color: Colors.white),
-          ),
-        ),
-        body: ListView.separated(
-          padding: const EdgeInsets.all(8),
-          itemCount: id.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Container(
-              height: 95,
-              color: Colors.grey.shade800,
-              child: ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor:
-                      MaterialStateProperty.all(Colors.grey.shade800),
-                ),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const SecondRoute()),
-                  );
-                },
-                child: Row(
-                  children: [
-                    Container(
-                      margin: const EdgeInsets.all(10),
-                      child: Text(
-                        '${id[index]}, ${winery[index]}, ${area[index]} \n Vintage: ${vintage[index]} \n Release: ${release[index]} \n Price: ${price[index]}',
-                        style: const TextStyle(color: Colors.white),
-                        textAlign: TextAlign.start,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            );
-          },
-          separatorBuilder: (BuildContext context, int index) =>
-              const Divider(),
-        ),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-    );
-  }
-}
-
-class SecondRoute extends StatelessWidget {
-  const SecondRoute({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Wine Bottle'),
-      ),
-      body: ElevatedButton(
-        onPressed: () {
-          Navigator.pop(context);
-        },
-        child: const Text('Go back!'),
-      ),
+      initialRoute: '/GeneratedAddWineWidget',
+      routes: {
+        '/GeneratedCRUSCOTTODELLACANTINAWidget': (context) =>
+            GeneratedCRUSCOTTODELLACANTINAWidget(),
+        '/GeneratedFooterWidget': (context) => GeneratedFooterWidget(),
+        '/GeneratedAddWineWidget': (context) => GeneratedAddWineWidget(),
+        '/GeneratedEditWineWidget': (context) => GeneratedEditWineWidget(),
+        '/GeneratedWineWidget2': (context) => GeneratedWineWidget2(),
+        '/GeneratedWineListWidget': (context) => GeneratedWineListWidget(),
+        '/GeneratedWineBottleWidget6': (context) =>
+            GeneratedWineBottleWidget6(),
+        '/GeneratedFooterWidget5': (context) => GeneratedFooterWidget5(),
+        '/GeneratedHomeHeaderWidget': (context) => GeneratedHomeHeaderWidget(),
+      },
     );
   }
 }
